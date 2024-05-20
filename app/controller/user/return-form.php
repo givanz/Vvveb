4f0edf2fb7e00c715a6e24aeca235d1828d4b219
givanz <givanz@yahoo.com>
2024-05-20 15:34:52 +0300

    Return form

--- app/controller/user/return-form.php
+++ app/controller/user/return-form.php
@@ -22,10 +22,57 @@
 
 namespace Vvveb\Controller\User;
 
+use function Vvveb\__;
 use Vvveb\Controller\Base;
+use function Vvveb\session as sess;
+use Vvveb\Sql\OrderSQL;
 use Vvveb\Sql\Return_ReasonSQL;
+use Vvveb\Sql\ReturnSQL;
 
 class ReturnForm extends Base {
+	function save() {
+		$customer_order_id      = $this->request->post['customer_order_id'] ?? false;
+		$email                  = $this->request->post['email'] ?? '';
+		$csrf                   = $this->request->post['csrf'] ?? false;
+		$validCsrf              = sess('csrf');
+		$this->view->validOrder = false;
+
+		if ($validCsrf === $csrf) {
+			if ($customer_order_id && $email) {
+				$orders  = new OrderSQL();
+				$results = $orders->get(['customer_order_id' => $customer_order_id, 'email' => $email]);
+
+				if ($results && $results['order']) {
+					$this->request->post['order_id']             = $results['order']['order_id'];
+					$this->request->post['return_resolution_id'] =  1;
+					$this->request->post['return_status_id']     =  1;
+					$this->request->post['product_id']           =  1;
+				} else {
+					$this->view->errors['return'] = sprintf(__('Order %s not found!'), $customer_order_id);
+
+					return $this->index();
+				}
+
+				$data    = $this->request->post + $this->global;
+				$return  = new ReturnSQL();
+				$results = $return->add(['return' => $data]);
+
+				if ($results && $results['return']) {
+					$this->view->success['return'] = __('Return submitted!');
+					$this->view->validOrder        = true;
+				} else {
+					$this->view->errors['return'] = __('Error submitting return!');
+				}
+			} else {
+				$this->view->errors['return'] = __('Order id and email not provided!');
+			}
+		} else {
+			$this->view->errors[] = __('Invalid request!');
+		}
+
+		return $this->index();
+	}
+
 	function index() {
 		$reason  = new Return_ReasonSQL();
 		$reasons = $reason->getAll($this->global + ['start' => 0, 'limit' => 100])['return_reason'] ?? [];
--- app/template/user/return-form.tpl
+++ app/template/user/return-form.tpl
@@ -1,5 +1,9 @@
 //keep post values
-input|value = <?php if (isset($_POST['@@__name__@@'])) echo $_POST['@@__name__@@']; else echo '@@__value__@@';?>
+input[type="radio"]|addNewAttribute = <?php if (isset($_POST['@@__name__@@']) && $_POST['@@__name__@@'] == '@@__value__@@') echo 'checked';?>
+input[type="checkbox"]|addNewAttribute = <?php if (isset($_POST['@@__name__@@']) && $_POST['@@__name__@@'] == '@@__value__@@') echo 'checked';?>
+input[type="text"]|value = <?php if (isset($_POST['@@__name__@@'])) echo $_POST['@@__name__@@']; else echo '@@__value__@@';?>
+input[type="email"]|value = <?php if (isset($_POST['@@__name__@@'])) echo $_POST['@@__name__@@']; else echo '@@__value__@@';?>
+textarea = <?php if (isset($_POST['@@__name__@@'])) echo $_POST['@@__name__@@']; else echo '@@__value__@@';?>
 
 import(common.tpl)
 
@@ -14,13 +18,15 @@ import(common.tpl)
 @reason = [data-v-return_reason] [data-v-option]
 [data-v-return_reason] [data-v-option]|deleteAllButFirstChild
 [data-v-return_reason] [data-v-option]|before = <?php 
-    if (isset($this->return_reason_id))
-	foreach ($this->return_reason_id as $value => $text) {
+    if (isset($this->return_reason_id)) {
+		$selected = $_POST['return_reason_id'] ?? '';
+		foreach ($this->return_reason_id as $value => $text) {
+			
 	?>
 
-	@reason|value = $value
-	// @reason|addNewAttribute = <?php if ($value == $selected) echo 'selected';?>
+	@reason [name="return_reason_id"]|value = $text['return_reason_id']
+	@reason [name="return_reason_id"]|addNewAttribute = <?php if ($text['return_reason_id'] == $selected) echo 'checked';?>
 	@reason span = <?php if (is_array($text)) { if (isset($text['name'])) echo Vvveb\humanReadable($text['name']);} else echo $text;?>  
 
 @reason|after = <?php 
-} ?>
+} }?>
