98ae9939dbf755aa645d3134ed42ecd974698679
givanz <givanz@yahoo.com>
2024-06-12 01:02:45 +0300

    Added new 'paid' payment status and 'shipped' shipping status

--- install/sql/insert/order/payment_status.sql
+++ install/sql/insert/order/payment_status.sql
@@ -2,8 +2,9 @@ INSERT INTO `payment_status` VALUES
 (1,1,'not_paid'),
 (2,1,'awaiting'),
 (3,1,'captured'),
-(4,1,'canceled'),
-(5,1,'refunded'),
-(6,1,'partially_refunded'),
-(7,1,'chargeback'),
-(8,1,'requires_action');
+(4,1,'paid'),
+(5,1,'canceled'),
+(6,1,'refunded'),
+(7,1,'partially_refunded'),
+(8,1,'chargeback'),
+(9,1,'requires_action');
