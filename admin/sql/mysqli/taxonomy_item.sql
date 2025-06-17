61fe7232710ebef2fe60d38b3bb3d66f8ca986bc
givanz <givanz@gmail.com>
2025-06-17 18:58:08 +0300

    Added slug, language and site id parameters to taxonomy methods

--- admin/sql/mysqli/taxonomy_item.sql
+++ admin/sql/mysqli/taxonomy_item.sql
@@ -4,15 +4,35 @@
 	
 	PROCEDURE getAll(
 		IN language_id INT,
+		IN site_id INT,
+		IN parent_id INT,
 		IN start INT,
 		IN limit INT,
+
+		-- filter
+		IN post_type CHAR,
+		IN type CHAR,
+
 		OUT fetch_all, 
 		OUT fetch_one,
 	)
 	BEGIN
 		-- taxonomy_item
 		SELECT * FROM taxonomy_item 
-			WHERE 1 = 1
+			LEFT JOIN taxonomy_to_site t2s ON (taxonomy_item.taxonomy_item_id = t2s.taxonomy_item_id AND site_id = :site_id) 
+			LEFT JOIN taxonomy_item_content t2c ON (taxonomy_item.taxonomy_item_id = t2c.taxonomy_item_id AND t2c.language_id = :language_id ) 
+		WHERE 1 = 1
+
+		@IF isset(:post_type) AND isset(:type)
+		THEN 
+			AND taxonomy_id = (SELECT taxonomy_id FROM taxonomy WHERE post_type = :post_type AND type = :type LIMIT 1)
+		END @IF			
+		
+		@IF isset(:parent_id)
+		THEN 
+			AND parent_id = :parent_id
+		END @IF		
+		
 		
 		@SQL_LIMIT(:start, :limit);
 		
@@ -28,11 +48,32 @@
 
 	PROCEDURE get(
 		IN taxonomy_item_id INT,
+		IN language_id INT,
+		IN slug CHAR,
 		OUT fetch_row, 
 	)
 	BEGIN
 		-- taxonomy_item
-		SELECT * FROM taxonomy_item as _ WHERE taxonomy_item_id = :taxonomy_item_id;
+		SELECT * FROM taxonomy_item as _ 
+			LEFT JOIN taxonomy_item_content t2c ON (_.taxonomy_item_id = t2c.taxonomy_item_id AND t2c.language_id = :language_id ) 
+		WHERE  1 = 1
+
+		@IF isset(:taxonomy_item_id)
+		THEN 
+		
+			AND _.taxonomy_item_id = :taxonomy_item_id
+
+		END @IF			
+		
+		@IF isset(:slug)
+		THEN 
+		
+			AND t2c.slug = :slug
+
+		END @IF
+		
+		LIMIT 1;
+
 	END
 	
 	-- add taxonomy_item
