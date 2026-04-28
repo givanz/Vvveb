9ff0da06985e5e89167030e496f9d6c37efb31bf
KishultanLab <kishultan@outlook.com>
2026-04-28 10:32:21 +0800

    Fix installer crashes and MySQL ANSI_QUOTES compatibility

--- install/sql/mysqli/schema/media/media_content.sql
+++ install/sql/mysqli/schema/media/media_content.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS `media_content`;
 CREATE TABLE `media_content` (
   `media_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `language_id` INT UNSIGNED NOT NULL,
-  `name` varchar(191) NOT NULL DEFAULT "",
-  `caption` varchar(191) NOT NULL DEFAULT "",
-  `description` varchar(191) NOT NULL DEFAULT "",
+  `name` varchar(191) NOT NULL DEFAULT '',
+  `caption` varchar(191) NOT NULL DEFAULT '',
+  `description` varchar(191) NOT NULL DEFAULT '',
 --  `content` longtext,
   PRIMARY KEY (`media_id`,`language_id`),
 --  KEY `alt` (`alt`),
