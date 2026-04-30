917c7347431fa571df5cf33f850834dacc78a6fb
givanz <givanz@gmail.com>
2026-04-30 18:45:57 +0300

    Added .phtml and .htaccess extensions to media upload deny list https://github.com/givanz/Vvveb/security/advisories/GHSA-qpcx-gx2x-r8v2 reported by https://github.com/CyberWarrior9

--- system/traits/media.php
+++ system/traits/media.php
@@ -30,7 +30,7 @@ use function Vvveb\sanitizeFileName;
 use Vvveb\System\Event;
 
 trait Media {
-	public $uploadDenyExtensions = ['php', 'svg', 'js', 'exe', 'html', 'phtml'];
+	public $uploadDenyExtensions = ['php', 'svg', 'js', 'exe', 'html', 'phtml', 'htaccess'];
 
 	public $uploadDenyMime    = ['image/svg', 'image/svg+xml', 'application/javascript', 'application/x-msdownload'];
 
