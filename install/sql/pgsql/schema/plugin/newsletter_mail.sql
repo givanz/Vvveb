a0d8ac52a547383acdf5f94b6e39814a24a76979
karlstevens <github@northsun.ca>
2026-05-02 21:28:41 -0600

    Various fixes to make these conform to Postgresql datatypes.
    Used 'text' instead of 'longtext' should work; Mysql longtext is 4GB
    while Postgresql text is 1GB.   I'm assuming that 4GB is used for Mysql
    as the next shortest (mediumtext) is 16MB.

--- install/sql/pgsql/schema/plugin/newsletter_mail.sql
+++ install/sql/pgsql/schema/plugin/newsletter_mail.sql
@@ -1,14 +1,14 @@
 -- DROP TABLE IF EXISTS "newsletter_mail";
 
 CREATE TABLE IF NOT EXISTS "newsletter_mail" (
-  "newsletter_list_id" INT UNSIGNED,
+  "newsletter_list_id" bigint,
   "email" varchar(254) NOT NULL,
   "first_name" varchar(254) NOT NULL DEFAULT '',
   "last_name" varchar(254) NOT NULL DEFAULT '',
   "data" text NOT NULL,
-  "status" smallint(6) NOT NULL DEFAULT '0', -- inactive = 0, active = 1, unsubsubscribed = 2 
-  "created_at" datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  "status" smallint NOT NULL DEFAULT 0, -- inactive = 0, active = 1, unsubsubscribed = 2 
+  "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX "newsletter_mail_email_newsletter_list_id" ON post ("email","newsletter_list_id");
+CREATE INDEX "newsletter_mail_email_newsletter_list_id" ON newsletter_mail ("email","newsletter_list_id");
