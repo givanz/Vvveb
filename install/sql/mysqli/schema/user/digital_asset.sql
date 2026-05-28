9d94efb5d2dc3f116dfac4f691b6a4a3994fa710
givanz <givanz@gmail.com>
2026-05-28 21:16:59 +0300

    Added admin_id column for digital assets

--- install/sql/mysqli/schema/user/digital_asset.sql
+++ install/sql/mysqli/schema/user/digital_asset.sql
@@ -2,8 +2,10 @@ DROP TABLE IF EXISTS `digital_asset`;
 
 CREATE TABLE `digital_asset` (
   `digital_asset_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `admin_id` INT unsigned NOT NULL COMMENT 'The id for the author of the asset',
   `file` varchar(160) NOT NULL,
   `public` varchar(128) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  KEY `admin` (`admin_id`),
   PRIMARY KEY (`digital_asset_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
--- install/sql/sqlite/schema/user/digital_asset.sql
+++ install/sql/sqlite/schema/user/digital_asset.sql
@@ -2,8 +2,11 @@ DROP TABLE IF EXISTS `digital_asset`;
 
 CREATE TABLE `digital_asset` (
 `digital_asset_id` INTEGER PRIMARY KEY AUTOINCREMENT,
+`admin_id` INT NOT NULL,
 `file` TEXT NOT NULL,
 `public` TEXT NOT NULL,
 `created_at` datetime NOT NULL
 -- PRIMARY KEY (`digital_asset_id`)
 );
+
+CREATE INDEX `digital_asset_admin` ON `post` (`admin_id`);
